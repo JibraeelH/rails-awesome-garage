d6617680bba4447daa389338196a4baeaf18bb2a
Jibraeel Hassen <jibraeel-hassen@hotmail.co.uk>
2024-10-03 14:23:39 +0100

    Models, validations &s seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Car.destroy_all
+Owner.destroy_all
+
+owner1 = Owner.create(nickname: "Geoffrey")
+owner2 = Owner.create(nickname: "Billy")
+
+Car.create(brand: "Mercedes-Benz", model: "G-Class", year: 2010, fuel: "Petrol", owner: owner1)
+Car.create(brand: "Mercedes-Benz", model: "A-Class", year: 2016, fuel: "Diesel", owner: owner2)
